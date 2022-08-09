4fd79327c74d0e6ba1b134a5a705acfc983407b0
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-09 03:02:51 +0000

    modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ViewsortComponent } from './viewsort/viewsort.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SortComponent } from './viewsort/sort/sort.component';
+import { CanvasComponent } from './viewsort/canvas/canvas.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ViewsortComponent,
+    SortComponent,
+    CanvasComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
+    MatInputModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
